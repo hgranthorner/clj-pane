477c3e1219bc6371ab2f0d0f37b774286543406d
Grant Horner <h.grant.horner@gmail.com>
2021-11-07 14:12:51 -0500

    set up more event listeners

--- src/core.clj
+++ src/core.clj
@@ -2,34 +2,44 @@
   (:require [seesaw.core :as ss]
             [seesaw.dev :refer (show-events show-options)])
   (:import [javax.swing UIManager]
+           [javax.swing.event DocumentEvent]
            [org.pushingpixels.radiance.theming.api.skin RadianceTwilightLookAndFeel]))
-
-(show-events (ss/label))
 
 (defn -main [& _]
   (ss/native!)
   (ss/invoke-later
    (UIManager/setLookAndFeel (RadianceTwilightLookAndFeel.))
-   (let [btn (ss/button :text "push me"
+   (let [text-input (ss/text :editable? true)
+         btn (ss/button :text "push me"
                         :id :the-button)
          list-box (ss/listbox :model ["This" "is" "a" "vertical" "stack of" "JLabels"])
-         v-panel (ss/vertical-panel :items [list-box btn])]
-     (ss/listen
-      btn
-      :action
-      (fn [_]
-        (.addElement (.getModel list-box) "another thing")))
+         v-panel (ss/vertical-panel
+                  :items [text-input
+                          list-box
+                          btn
+                          (ss/combobox :model ["a" "b" "c"]
+                                       :editable? true)])]
+     (ss/listen btn
+                :action (fn [_]
+                          (.addElement
+                           (.getModel list-box)
+                           "another thing")))
+
+     (ss/listen text-input
+                :document (fn [^DocumentEvent e]
+                            (let [d (.getDocument e)]
+                              (println (.getText d 0 (.getLength d))))))
      (->
       (ss/frame
        :title "Example"
-       :content v-panel)
+       :content v-panel
+       :on-close :dispose)
       ss/pack!
+      (ss/move-to! 0 0)
       ss/show!))))
 
 (comment
   (-main)
-  (show-options (ss/vertical-panel))
-  (show-options (ss/button))
-  (show-options (ss/action)))
-
-
+  (show-events (ss/text))
+  (show-options (ss/frame))
+  (show-options (ss/vertical-panel)))
